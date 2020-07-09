76695dae5082e8c0c5b96d321cce43796f5c7dfc
Devian Nightgale <deviancorps@gmail.com>
2020-07-09 14:26:13 -0700

    Wrote a small function to return all files matching a specific file extension

--- os_sys.py
+++ os_sys.py
@@ -1,6 +1,19 @@
 
 
 import os
+import re
+
+def find_file_extension(extension,file_list):
+	search_expression="*."+extension.lower()
+	matches=[]
+	for file in file_list:
+		x=re.search(search_expression,file)
+		if x:
+			matches.append(file)
+	return matches
+
+
+
 
 directory_contents=os.listdir()
 print(directory_contents)
